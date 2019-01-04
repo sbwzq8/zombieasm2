1141c7e84d06b9f39b6bc2192f8458453d403ee3
sbwzq8 <sbwzq8@mail.missouri.edu>
2019-01-04 00:47:12 -0600

    Fixed typo in cipher text

--- zombiesasm2/main.cpp
+++ zombiesasm2/main.cpp
@@ -3,32 +3,12 @@
 
 using namespace std;
 
-//extern "C" { 
-//	int main2();
-//}
-//extern "C" char* zomb();
 extern "C" char* zomb();
 
 int main() {
 
 	char* value = zomb();
 	cout << "Message: " << value << endl;
-	//int address = (int)&value;
-	//cout << "Testing: " << &value << endl;
-	//cout << "Int: " << address << endl;
-	//value++;
-	//cout << "Testing: " << &value << endl;
-	//cout << "Int: " << address << endl;
-
-
-	//cout << "The result is: " << zomb() << endl;
-	/*char* stuff = zomb();
-	cout << "The result is: " << (char)&stuff << endl;
-	for (int i = 0; i < 64; i++) {
-		cout << "The value of pointer is " << (int)stuff << endl;
-		cout << (char)stuff << endl;
-		stuff++;
-	}*/
 
 	return 0;
 }
